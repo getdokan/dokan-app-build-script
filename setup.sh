42ec6ec7af66d8b39b62a14c541ffa8d4756fc8c
ibnulK <ikayes114@gmail.com>
2019-07-18 12:34:23 +0600

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -3,31 +3,29 @@
 if [ ! -f /usr/local/bin/config-android ]
 then
   printf "\n"
-  echo "Installing Dokan Android app configuration script"
-  printf "\n"
+  echo -e "Installing Dokan Android app configuration script\n"
   wget -qO /usr/local/bin/config-android https://raw.githubusercontent.com/weDevsOfficial/dokan-app-build-script/master/configure-android.sh
 
   # Executable permission
   chmod a+x /usr/local/bin/config-android
 
-  echo "Done!"
+  echo -e "Done!\n"
 else
-  echo "Android configuration script is already installed"
+  echo -e "Android configuration script is already installed\n"
 fi
 
 if [ ! -f /usr/local/bin/build-android ]
 then
-  printf "\n"
-  echo "Installing Dokan Android app build script"
-  printf "\n"
+  echo -e "Installing Dokan Android app build script\n"
+
   wget -qO /usr/local/bin/build-android https://raw.githubusercontent.com/weDevsOfficial/dokan-app-build-script/master/build-android.sh
 
   # Executable permission
   chmod a+x /usr/local/bin/build-android
 
-  echo "Done!"
+  echo -e "Done!\n"
 else
-  
+  echo -e "Android build script is already installed\n"
 fi
 
 echo "**************************"
