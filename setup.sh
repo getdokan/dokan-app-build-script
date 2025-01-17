f3e04fdebf132add6955de885286c480bfe6b2ae
7ANV1R <tanvir.officemail@gmail.com>
2025-01-17 16:13:34 +0600

    Update setup script message for Dokan Cloud Customer app configuration

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ GREEN='\033[1;32m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-echo -e "${BLUE}==> Installing Dokan Android app configuration script${NC}"
+echo -e "${BLUE}==> Installing Dokan Cloud Customer app configuration script${NC}"
 if [[ ! -f /usr/local/bin/config-customer-app ]]; then
   wget -qO /usr/local/bin/config-customer-app https://raw.githubusercontent.com/weDevsOfficial/dokan-app-build-script/dokan_cloud_customer/config-customer-app.sh
   # Executable permission
